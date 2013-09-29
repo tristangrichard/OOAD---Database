4ad26f400c766efbdf412a59934b091793f496eb
Tristan Richard <tristangrichard@gmail.com>
2013-09-29 07:40:55 +0200

    Create User in Progress

--- GameWorld/src/funktionalitet/IUserLogic.java
+++ GameWorld/src/funktionalitet/IUserLogic.java
@@ -3,14 +3,14 @@ package funktionalitet;
 import java.util.List;
 
 import daointerfaces.DALException;
-import dto.BrugerDTO;
+import dto.UsersDTO;
 
 public interface IUserLogic {
 	public boolean isAdmin(int id) throws DALException;
-	public String createUser(String userName, String birth, String rolle, String email, int sex) throws DALException;
+	public String createUser(String fName, String lName, String birth, String rolle, String email, int sex) throws DALException;
 	public void updateOpr(int oprID, String oprName, String cpr, String oldPassword, String newPassword, String newPassword2, String rolle) throws DALException;
 	public void updateOprAdmin(int oprID, String oprName, String cpr,  String newPassword, String newPassword2, String rolle) throws DALException;
 	public void deleteOpr(int currentUser, int oprID) throws DALException;
-	public BrugerDTO getOperatoer(int oprID) throws DALException;
-	public List<BrugerDTO> getOperatoerList() throws DALException;
+	public UsersDTO getOperatoer(int oprID) throws DALException;
+	public List<UsersDTO> getOperatoerList() throws DALException;
 }
