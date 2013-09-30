5c1b4683979b1c8ed9b67d6805b33fcb24935b54
Tristan Richard <tristangrichard@gmail.com>
2013-09-30 17:13:41 +0200

    T6

--- GameWorld/src/Test/TestLauncher.java
+++ GameWorld/src/Test/TestLauncher.java
@@ -5,7 +5,8 @@ public class TestLauncher {
 	private static int errors = 0;
 	private static int tests = 0;
 	
-	TestDB tdb = new TestDB();
+	Test_User_Game tdb = new Test_User_Game();
+	Test_User_Game_Lists tdbl = new Test_User_Game_Lists();
 	TestBL tbl = new TestBL();
 	TestStatistics ts = new TestStatistics();
 	
@@ -25,6 +26,7 @@ public class TestLauncher {
 	public void TestLauncherStart(){
 		
 		tdb.TestDBStart();
+		tdbl.TestDBStart();
 		tbl.TestBLStart();
 		ts.TestStatisticsStart();
 		System.out.println("Test finished");
