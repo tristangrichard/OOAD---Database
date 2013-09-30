42035668c8ce9644592b713472569fd044ab2a9e
Tristan Richard <tristangrichard@gmail.com>
2013-09-30 19:26:45 +0200

    Unused imports in MyGamesLogic Removed

--- GameWorld/src/funktionalitet/MyGamesLogic.java
+++ GameWorld/src/funktionalitet/MyGamesLogic.java
@@ -2,22 +2,14 @@ package funktionalitet;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-
 import connector.Connector;
 import daoimpl.MySQLGameDAO;
-import daoimpl.MySQLRoleDAO;
-import daoimpl.MySQLUsersDAO;
 import daoimpl.MySQLUsersGamesDAO;
 import daointerfaces.DALException;
 import daointerfaces.GameIDAO;
-import daointerfaces.RoleIDAO;
 import daointerfaces.UsersGamesIDAO;
-import daointerfaces.UsersIDAO;
-import daointerfaces.UsersLangIDAO;
 import dto.GameDTO;
-import dto.UsersDTO;
 import dto.UsersGamesDTO;
 
 public class MyGamesLogic implements IMyGamesLogic {
