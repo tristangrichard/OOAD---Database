ccbf9200f3a8a900a1383daa39760b1302632d67
Mikkel Barfred <m.barfred1984@gmail.com>
2013-10-03 11:45:23 +0200

    test BL

--- GameWorld/src/Test/TestBL.java
+++ GameWorld/src/Test/TestBL.java
@@ -1,5 +1,11 @@
 package Test;
 
+import java.util.List;
+
+import daoimpl.MySQLPublisherDAO;
+import daointerfaces.PublisherIDAO;
+import dto.PublisherDTO;
+
 public class TestBL {
 
 	private boolean error = false;
@@ -15,18 +21,29 @@ public class TestBL {
 		//}																		//
 		//finally {TestLauncher.printProgress(error);}							//
 		//////////////////////////////////////////////////////////////////////////
+	PublisherIDAO pidao = new MySQLPublisherDAO();
 	
-	
+
 	
 	public void TestBLStart(){
 	
+	publisherGamesGraphAble();
+		
+		
 	}
 	
+	public void publisherGamesGraphAble(){
+		try{
+			List<PublisherDTO> pdtoL = pidao.getList();
+			}
+			catch(Exception e){
+				
+			}
+		
+		
+	}
 	//define methods for testing BusinessLogic
 	
-	
-	
-	
-	
-	
 }
+	
+	
