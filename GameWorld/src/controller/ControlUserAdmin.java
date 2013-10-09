4de6a8ebc2560a80ec9c310f4e884bec9dd8f6ff
Tristan Richard <tristangrichard@gmail.com>
2013-10-09 11:31:51 +0200

    Removed dead code

--- GameWorld/src/controller/ControlUserAdmin.java
+++ GameWorld/src/controller/ControlUserAdmin.java
@@ -230,8 +230,8 @@ public class ControlUserAdmin extends HttpServlet {
 				int iSex = Integer.parseInt(sex);
 				int iLang = Integer.parseInt(lang);	
 				if (role.equalsIgnoreCase("user") || role.equalsIgnoreCase("administrator")) {
-				userLogic.updateOprAdmin(fName, lName, birth, email, iSex, iLang, role, pass1, pass2);
-				request.setAttribute("message", "User with email: " + email + " successfully updated.");
+					userLogic.updateOprAdmin(fName, lName, birth, email, iSex, iLang, role, pass1, pass2);
+					request.setAttribute("message", "User with email: " + email + " successfully updated.");
 				} else {
 					String publisher = request.getParameter("newPub");
 					int iPublisher = Integer.parseInt(publisher);
@@ -255,84 +255,6 @@ public class ControlUserAdmin extends HttpServlet {
 				request.getRequestDispatcher("../WEB-INF/admin/index.jsp?").forward(request, response);
 			}
 		}
-
-		//			int oprIDToUpdate =  0;
-		//			// Setting the oprToUpdate to the current operator if no operator has been selected.
-		//			if (request.getParameter("oprIDToUpdate") == null) {
-		//				oprIDToUpdate = user.getOprId();
-		//			} else {
-		//				try {
-		//					oprIDToUpdate = Integer.parseInt(request.getParameter("oprIDToUpdate"));
-		//				} catch (NumberFormatException e) {
-		//					e.printStackTrace();
-		//					request.setAttribute("error", "Attribute oprToUpdate could not be parsed as an integer.");
-		//					request.getRequestDispatcher("../WEB-INF/opr/index.jsp?").forward(request, response);
-		//				}
-		//			}
-		//			if (oprIDToUpdate != 0) {
-		//				try {
-		//					// Creating a User bean that is to hold the information about the user to be updated.
-		//					BrugerDTO userToUpdate = userLogic.getOperatoer(oprIDToUpdate);
-		//					session.setAttribute("userToUpdate", userToUpdate);
-		//					request.getRequestDispatcher("../WEB-INF/opr/updateOpr.jsp?").forward(request, response); // Send the request to the update operator file.
-		//				} catch (DALException e) {
-		//					e.printStackTrace();
-		//					request.setAttribute("error", e.getMessage());
-		//					request.getRequestDispatcher("../WEB-INF/opr/index.jsp?").forward(request, response);
-		//				}
-		//			} else {
-		//				request.getRequestDispatcher("../WEB-INF/opr/index.jsp?").forward(request, response);
-		//			}
-		//		}
-		//		// Action: Delete operator. Deletion is made from the operator list.
-		//		// NOTE: This does not actually delete the operator, it deactivates it.
-		//		// Redirects to index.jsp.
-		//		else if ("deleteOpr".equals(action)) {
-		//			try {
-		//				int oprToDelete = Integer.parseInt(request.getParameter("oprIDToDelete"));
-		//				userLogic.deleteOpr(user.getOprId(), oprToDelete);
-		//				request.setAttribute("message", "Operator with ID: " + oprToDelete + " successfully deactivated.");
-		//			} catch (DALException e) {
-		//				e.printStackTrace();
-		//				request.setAttribute("error", e.getMessage());
-		//			} catch (NumberFormatException e) {
-		//				e.printStackTrace();
-		//				request.setAttribute("error", "Attribute oprIDToDelete could not be parsed as an integer.");
-		//			} finally {
-		//				request.getRequestDispatcher("index.jsp?action=oprList").forward(request, response);
-		//			}
-		//		} 
-		//		// Action: Carry out the changes made in the filled update operator form.
-		// Redirects to index.jsp.
-		//		else if ("updateOprFilled".equals(action)) {
-		//			// Getting all the details from the filled form.
-		//			try {
-		//				int updOprID = Integer.parseInt(request.getParameter("oprIDToUpdate"));
-		//				String updOprName = request.getParameter("updOprName");
-		//				String updOprCpr = request.getParameter("updOprCpr");
-		//				String updOprPass1 = request.getParameter("updOprPass1");
-		//				String updOprPass2 = request.getParameter("updOprPass2");
-		//				String updOprPassOld = request.getParameter("updOprPassOld");
-		//				String updOprRole = request.getParameter("updOprRole");
-		//
-		//				if (!userLogic.isAdmin(user.getOprId())) { // Use the normal method if user is not an admin.
-		//					userLogic.updateOpr(updOprID, updOprName, updOprCpr, updOprPassOld, updOprPass1, updOprPass2, updOprRole);
-		//				} else { // Use the admin method that does not require the old password if the user is an admin.
-		//					userLogic.updateOprAdmin(updOprID, updOprName, updOprCpr, updOprPass1, updOprPass2, updOprRole);
-		//				}
-		//				request.setAttribute("message", "Operator with ID: " + updOprID + " successfully updated.");
-		//				request.getRequestDispatcher("index.jsp?action=oprList").forward(request, response);
-		//			} catch (DALException e) {
-		//				e.printStackTrace();
-		//				request.setAttribute("error", e.getMessage());
-		//				request.getRequestDispatcher("index.jsp?action=updateOpr").forward(request, response);
-		//			} catch (NumberFormatException e) {
-		//				e.printStackTrace();
-		//				request.setAttribute("error", "Attribute oprIDToUpdate could not be parsed as an integer.");
-		//				request.getRequestDispatcher("index.jsp?action=updateOpr").forward(request, response);
-		//			}
-		//		} 
-		// Action: default action if no action is set.
 		else { 
 			request.getRequestDispatcher("../WEB-INF/admin/index.jsp?").forward(request, response);
 		}
